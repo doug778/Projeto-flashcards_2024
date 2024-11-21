74b4dfc277994dc1b82500cf00e3c3fe78448420
doug778 <162638137+doug778@users.noreply.github.com>
2024-11-21 17:29:03 -0300

    Add files via upload

---
+++ Projeto-2024_flashcard/perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Ficção',
+    'Quantos epísodios possui a obra One Piece?',
+    'Neste momento, possui 1122 epísodios, sem contar seus especiais e filmes'
+)   
+criaCartao(
+    'Ficção',
+    'Quem é o criador de Dragon ball?',
+    'Seu criador é Akira Toriyama (1955-2024)'
+)
